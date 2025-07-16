47e2c9fb2a3190a401fc632bc1f60b894d05ce10
Małgorzata Dudziec <malgorzata.dudziec@currenda.pl>
2025-07-16 10:22:36 +0200

    Cache API

--- layers/chat/server/api/chats/index.post.ts
+++ layers/chat/server/api/chats/index.post.ts
@@ -2,6 +2,10 @@ import { createChat } from '../../repository/chatRepository';
 
 export default defineEventHandler(async (event) => {
   const { title, projectId } = await readBody(event);
+
+  const storage = useStorage('db');
+  await storage.setItem('chats:has-new-chat', true);
+
   return createChat({
     title,
     projectId
